d7c81ac5367053e7767123ddc9ec9244d1c48788
Hirandalpe <65526913+Hirandalpe@users.noreply.github.com>
2023-10-26 11:30:09 +0530

    System Level Proxy rotation
    System Proxy rotation in windows PC using Python.

---
+++ systemprox.py
@@ -0,0 +1,16 @@
+import os
+import ctypes
+import winreg as wreg
+
+#Disabling proxy
+
+def disable_proxy():
+    try:
+        key = wreg.OpenKey(wreg.HKEY_CURRENT_USER, r"Software\Microsoft\Windows\CurrentVersion\Internet Settings", 0,wreg.KEY_WRITE)
+        wreg.SetValueEx(key, "ProxyEnable", 0, wreg.REG_DWORD, 0)
+        wreg.CloseKey(key)
+        ctypes.windll.Wininet.InternetSetOptionW(0, 81, 0, 0)
+    except Exception as e:
+        print("Error!!", e)
+
+
